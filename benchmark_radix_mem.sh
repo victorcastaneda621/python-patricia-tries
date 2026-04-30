215e521c6004913d1c8de4f19a5f83a01504d9a7
Víctor Castañeda Balmori <victor.castaneda@alumnos.unican.es>
2026-04-30 18:36:45 +0200

    Alt benchmark

--- benchmark_radix_mem.sh
+++ benchmark_radix_mem.sh
@@ -2,44 +2,34 @@
 #SBATCH -J patricia_mine          
 #SBATCH -o logs/bench_%A_%a.out   
 #SBATCH -e logs/bench_%A_%a.err   
-#SBATCH -t 24:00:00               
+#SBATCH -t 48:00:00               
 #SBATCH --mem-per-cpu=4096        
 #SBATCH -n 1                      
 #SBATCH -c 1
-#SBATCH --array=0-5
+#SBATCH --array=0-3
 
 mkdir -p logs
 
 case $SLURM_ARRAY_TASK_ID in
     0)
-        ALG="radix-SN-TD"
-        DATASET="mushroom"
-        MINSUP=650
+        ALG="radix-SN-BU"
+        DATASET="pumsb_star"
+        MINSUP=14000
         ;;
     1)
-        ALG="radix-SN-TD"
-        DATASET="mushroom"
-        MINSUP=550
+        ALG="radix-MN-BU"
+        DATASET="pumsb_star"
+        MINSUP=14000
         ;;
     2)
         ALG="radix-SN-TD"
-        DATASET="mushroom"
-        MINSUP=450
+        DATASET="artificial_1"
+        MINSUP=200
         ;;
     3)
-        ALG="radix-SN-TD"
-        DATASET="mushroom"
-        MINSUP=350
-        ;;
-    4)
-        ALG="radix-SN-TD"
-        DATASET="mushroom"
-        MINSUP=250
-        ;;
-    5)
-        ALG="radix-SN-TD"
-        DATASET="mushroom"
-        MINSUP=150
+        ALG="radix-MN-TD"
+        DATASET="artificial_1"
+        MINSUP=200
         ;;
     
 esac
