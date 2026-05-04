a83f4093fcc61d083bfb1ca4a5f6af5a291fa53d
Víctor Castañeda Balmori <victor.castaneda@alumnos.unican.es>
2026-05-04 12:40:43 +0200

    patricia benchmark

--- benchmark_patricia_topk.sh
+++ benchmark_patricia_topk.sh
@@ -2,7 +2,7 @@
 #SBATCH -J patricia_mine          
 #SBATCH -o logs/bench_%A_%a.out   
 #SBATCH -e logs/bench_%A_%a.err   
-#SBATCH -t 24:00:00               
+#SBATCH -t 48:00:00               
 #SBATCH --mem-per-cpu=4096        
 #SBATCH -n 1                      
 #SBATCH -c 1
@@ -14,27 +14,27 @@ case $SLURM_ARRAY_TASK_ID in
     0)
         ALG="patricia"
         DATASET="mushroom"
-        k=640
+        k=915325
         ;;
     1)
         ALG="patricia"
         DATASET="connect4"
-        k=40535
+        k=8368159
         ;;
     2)
         ALG="patricia"
         DATASET="pumsb"
-        k=35000
+        k=1897479
         ;;
     3)
         ALG="patricia"
         DATASET="pumsb_star"
-        k=14000
+        k=665683
         ;;
     4)
         ALG="patricia"
         DATASET="artificial_1"
-        k=200
+        k=13255
         ;;
     
 esac
@@ -42,6 +42,6 @@ esac
 
 echo "Starting: ID=$SLURM_ARRAY_TASK_ID | Algorithm=$ALG | Dataset=$DATASET | K=$k"
 
-python3 PatriciaMine.py --alg "$ALG" --data "$DATASET" --k "$k" --benchmark
+python3 TopKMiner.py --alg "$ALG" --data "$DATASET" --k "$k" --benchmark
 
 echo "Finished"
